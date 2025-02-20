98bf245ef87af40ed77387e20151b5cc19f31568
Filip Bujalski <bujalski.filip@szkola.elektrykplock.edu.pl>
2025-02-20 19:07:12 +0100

    Nowe dane i poprawki
    Poprawiłem tabele z zadaniami oraz wprowadziłem dane

--- Erasmus-2.sql
+++ Erasmus-2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jan 11, 2025 at 03:47 PM
+-- Generation Time: Feb 20, 2025 at 07:05 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -20,6 +20,32 @@ SET time_zone = "+00:00";
 --
 -- Database: `Erasmus`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `choice_tasks`
+--
+
+CREATE TABLE `choice_tasks` (
+  `id` int(11) NOT NULL,
+  `game_id` int(11) DEFAULT NULL,
+  `task_number` int(11) NOT NULL,
+  `coord_x` decimal(10,6) NOT NULL,
+  `coord_y` decimal(10,6) NOT NULL,
+  `points` int(11) NOT NULL,
+  `question` varchar(255) NOT NULL,
+  `options` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`options`)),
+  `correct_option_index` int(11) NOT NULL,
+  `hints` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`hints`))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `choice_tasks`
+--
+
+INSERT INTO `choice_tasks` (`id`, `game_id`, `task_number`, `coord_x`, `coord_y`, `points`, `question`, `options`, `correct_option_index`, `hints`) VALUES
+(1, 4, 5, 52.546007, 19.685939, 5, 'Co znajduje się na kopcu harcerza?', '[\"Krzyż walecznych\", \"Krzyż harcerski\", \"Krzyż Virtuti Militari\"]', 1, NULL);
 
 -- --------------------------------------------------------
 
@@ -69,7 +95,10 @@ CREATE TABLE `games` (
 INSERT INTO `games` (`id`, `author_id`, `title`, `creation_date`, `coord_x`, `coord_y`, `description`) VALUES
 (1, 2, 'Sylwester u Zduniaka', '2024-12-31', 53.066486, 21.149823, 'Impreza sylwestrowa ze wstępem wolnym dla każdego w Przytułach'),
 (2, 1, 'Rowerowe szaleństwo', '2025-01-13', 52.541332, 19.755332, 'Zapraszam wszystkich zainteresowanych do wspólnych szaleństw na Pump Tracku. Wszyscy mile widziani.'),
-(3, 3, 'Wyjście z domu', '2025-01-16', 52.559106, 19.695203, 'Organizuję wyjście po zajęciach lekcyjnych na kebaba. Inicjatywa ma na celu integrację z innymi osobami zamiast przesiadywania w domu i grania na komputerze.');
+(3, 3, 'Wyjście z domu', '2025-01-16', 52.559106, 19.695203, 'Organizuję wyjście po zajęciach lekcyjnych na kebaba. Inicjatywa ma na celu integrację z innymi osobami zamiast przesiadywania w domu i grania na komputerze.'),
+(4, 2, 'Sakralna Strona Miasta', '2025-02-14', 52.545007, 19.684939, 'Ta trasa poprowadzi Cię przez duchowe serce Płocka, odkrywając zarówno wielowiekowe świątynie, jak i miejsca pamięci związane z religijną historią miasta. Spacerując wśród zabytkowych budowli, poznasz losy społeczności, które na przestrzeni wieków kształtowały duchowy charakter Płocka. Wędrówka obejmie również punkty upamiętniające ważne postaci i wydarzenia, a także symboliczne miejsca, które do dziś przyciągają pielgrzymów i mieszkańców szukających chwili zadumy.'),
+(5, 2, 'Płock uszyty sztuką', '2025-02-14', 52.545007, 19.684939, 'Miasto, w którym historia splata się ze sztuką, odkryje przed Tobą swoje najbardziej malownicze i inspirujące zakątki. Podczas tej gry terenowej odnajdziesz ślady znanych twórców, poznasz miejsca, które stały się tłem dla filmowych opowieści, oraz napotkasz oryginalne instalacje artystyczne. Spacerując wzdłuż malowniczych krajobrazów i uliczek, zobaczysz, jak różnorodne formy sztuki wpisują się w przestrzeń miejską, tworząc unikalną atmosferę.'),
+(6, 2, 'Historyczne oblicze Płocka', '2025-02-14', 52.545007, 19.684939, 'Przenieś się w czasie i poznaj miejsca, które były świadkami kluczowych wydarzeń w dziejach miasta. Trasa poprowadzi Cię śladami dawnych fortyfikacji, miejsc pamięci oraz budowli, które przez wieki odgrywały istotną rolę w życiu mieszkańców. Spacerując ulicami Płocka, odkryjesz historie ludzi, którzy walczyli o wolność, tworzyli i bronili miasta, a także zobaczysz ślady dawnych epok, które do dziś kształtują jego niepowtarzalny charakter.');
 
 -- --------------------------------------------------------
 
@@ -94,7 +123,10 @@ INSERT INTO `game_tags` (`game_id`, `tag_id`) VALUES
 (2, 7),
 (2, 8),
 (3, 4),
-(3, 7);
+(3, 7),
+(4, 7),
+(5, 4),
+(6, 10);
 
 -- --------------------------------------------------------
 
@@ -116,6 +148,32 @@ INSERT INTO `login_details` (`id`, `login`, `password`) VALUES
 (1, 'wiktor', '6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b'),
 (2, 'filip', 'd4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35'),
 (3, 'pawel', '4e07408562bedb8b60ce05c1decfe3ad16b72230967de01f640b7e4729b49fce');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `number_tasks`
+--
+
+CREATE TABLE `number_tasks` (
+  `id` int(11) NOT NULL,
+  `game_id` int(11) DEFAULT NULL,
+  `task_number` int(11) NOT NULL,
+  `coord_x` decimal(10,6) NOT NULL,
+  `coord_y` decimal(10,6) NOT NULL,
+  `points` int(11) NOT NULL,
+  `question` varchar(255) NOT NULL,
+  `answer` int(11) NOT NULL,
+  `hints` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`hints`))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `number_tasks`
+--
+
+INSERT INTO `number_tasks` (`id`, `game_id`, `task_number`, `coord_x`, `coord_y`, `points`, `question`, `answer`, `hints`) VALUES
+(3, 6, 1, 52.546007, 19.685939, 5, 'Ile lwów znajduje się przed ratuszem?', 2, NULL),
+(4, 5, 2, 52.545007, 19.684939, 5, 'Ile postaci przedstawia mural nad murami?', 4, NULL);
 
 -- --------------------------------------------------------
 
@@ -143,6 +201,32 @@ INSERT INTO `tags` (`id`, `name`) VALUES
 (8, 'Wyczynowe'),
 (9, 'Nie zdrowe'),
 (10, 'Edukacyjne');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `text_tasks`
+--
+
+CREATE TABLE `text_tasks` (
+  `id` int(11) NOT NULL,
+  `game_id` int(11) DEFAULT NULL,
+  `task_number` int(11) NOT NULL,
+  `coord_x` decimal(10,6) NOT NULL,
+  `coord_y` decimal(10,6) NOT NULL,
+  `points` int(11) NOT NULL,
+  `question` varchar(255) NOT NULL,
+  `answer` varchar(255) NOT NULL,
+  `hints` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`hints`))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `text_tasks`
+--
+
+INSERT INTO `text_tasks` (`id`, `game_id`, `task_number`, `coord_x`, `coord_y`, `points`, `question`, `answer`, `hints`) VALUES
+(1, 4, 3, 52.545007, 19.684939, 5, 'Wpisz fragment tekstu na pomniku Broniewskiego', 'Z górskiej spoglądam na królewski las', NULL),
+(2, 6, 4, 52.546007, 19.685939, 5, 'Podaj trzy istotne słowa związane z Placem Trzynastu Straconych', 'chleb, pokój, wolność', NULL);
 
 -- --------------------------------------------------------
 
@@ -184,6 +268,13 @@ INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `phone_number`) V
 --
 
 --
+-- Indexes for table `choice_tasks`
+--
+ALTER TABLE `choice_tasks`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `game_id` (`game_id`);
+
+--
 -- Indexes for table `cities`
 --
 ALTER TABLE `cities`
@@ -210,10 +301,24 @@ ALTER TABLE `login_details`
   ADD KEY `login_details_ibfk_1` (`id`);
 
 --
+-- Indexes for table `number_tasks`
+--
+ALTER TABLE `number_tasks`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `game_id` (`game_id`);
+
+--
 -- Indexes for table `tags`
 --
 ALTER TABLE `tags`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `text_tasks`
+--
+ALTER TABLE `text_tasks`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `game_id` (`game_id`);
 
 --
 -- Indexes for table `tokens`
@@ -232,6 +337,12 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `choice_tasks`
+--
+ALTER TABLE `choice_tasks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `cities`
 --
 ALTER TABLE `cities`
@@ -241,13 +352,25 @@ ALTER TABLE `cities`
 -- AUTO_INCREMENT for table `games`
 --
 ALTER TABLE `games`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `number_tasks`
+--
+ALTER TABLE `number_tasks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `tags`
 --
 ALTER TABLE `tags`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `text_tasks`
+--
+ALTER TABLE `text_tasks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -258,6 +381,12 @@ ALTER TABLE `users`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `choice_tasks`
+--
+ALTER TABLE `choice_tasks`
+  ADD CONSTRAINT `choice_tasks_ibfk_1` FOREIGN KEY (`game_id`) REFERENCES `games` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `games`
@@ -277,6 +406,18 @@ ALTER TABLE `game_tags`
 --
 ALTER TABLE `login_details`
   ADD CONSTRAINT `login_details_ibfk_1` FOREIGN KEY (`id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `number_tasks`
+--
+ALTER TABLE `number_tasks`
+  ADD CONSTRAINT `number_tasks_ibfk_1` FOREIGN KEY (`game_id`) REFERENCES `games` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `text_tasks`
+--
+ALTER TABLE `text_tasks`
+  ADD CONSTRAINT `text_tasks_ibfk_1` FOREIGN KEY (`game_id`) REFERENCES `games` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `tokens`
